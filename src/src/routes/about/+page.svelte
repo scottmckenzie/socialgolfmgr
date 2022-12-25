d9d0b6111423d84d3a9fa5d9337b4a0488f20918
Scott McKenzie <scott@noizyland.net>
2022-12-25 18:56:27 +1100

    Move main style to app.css

--- src/src/routes/about/+page.svelte
+++ src/src/routes/about/+page.svelte
@@ -3,14 +3,3 @@
     <hr />
     <div>A website to find user profiles</div>
 </main>
-
-<style>
-    main {
-        font-size: 1.5rem;
-        margin: 4rem;
-        padding: 2rem;
-        color: gray;
-        justify-content: center;
-        box-shadow: 4px 5px 11px 10px lightgray;
-    }
-</style>
